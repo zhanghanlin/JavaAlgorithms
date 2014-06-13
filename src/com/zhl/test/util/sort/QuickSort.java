2eda1f052a2ec826a98ea746faa9f2cc0530575f
Andy <andyzhanghanlin@163.com>
2014-06-13 12:46:29 +0800

    快速排序

--- src/com/zhl/test/util/sort/QuickSort.java
+++ src/com/zhl/test/util/sort/QuickSort.java
@@ -14,6 +14,12 @@ public class QuickSort {
 	
 	/**
 	 * 快速排序
+	 * 伪代码
+	 * QUICKSORT(A, p, r)
+	 * 	if p < r
+	 * 		q = PARTITION(A, p, r)
+	 * 		QUICKSORT(A, p, q - 1)
+	 * 		QUICKSORT(A, q + 1, r)
 	 * @param src	需要排序的数组
 	 * @param p	排序开始位置
 	 * @param r	排序结束位置
@@ -28,6 +34,16 @@ public class QuickSort {
 	
 	/**
 	 * 返回数组分割下标位置
+	 * 伪代码
+	 * PARTITION(A, p, r)
+	 * 	x = A[r]
+	 * 	i = p - 1
+	 * 	for j = p to r - 1
+	 * 		if A[j] ≤ x
+	 * 			i = i + 1
+	 * 			exchange A[i] with A[j]
+	 * 	exchange A[i + 1] with A[r]
+	 * 	return i + 1
 	 * @param p	排序开始位置
 	 * @param r	排序结束位置
 	 * @return
@@ -47,6 +63,12 @@ public class QuickSort {
 	
 	/**
 	 * 快速排序随机化版本
+	 * 伪代码
+	 * RANDOMIZED-QUICKSORT(A, p, r)
+	 * 	if p < r
+	 * 		RANDOMIZED-PARTITION(A, p, r)
+	 * 		RANDOMIZED-QUICKSORT(A, p, q - 1)
+	 * 		RANDOMIZED-QUICKSORT(A, q + 1, r)
 	 * @param p
 	 * @param r
 	 */
@@ -60,6 +82,11 @@ public class QuickSort {
 	
 	/**
 	 * 返回数组分割下标位置 - 快速排序随机化版本
+	 * 伪代码
+	 * RANDOMIZED-PARTITION(A, p, r)
+	 * 	i = RANDOM(p, r)
+	 * 	exchange A[r] with A[i]
+	 * 	return PARTITION(A, p, r)
 	 * @param p	排序开始位置
 	 * @param r	排序结束位置
 	 * @return
