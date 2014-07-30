20bfc351744c707d6a17837200897f3b95e677dc
Andy <andyzhanghanlin@163.com>
2014-07-30 14:47:21 +0800

    插入排序

--- src/com/zhl/test/util/sort/InsertionSort.java
+++ src/com/zhl/test/util/sort/InsertionSort.java
@@ -8,38 +8,14 @@ import com.zhl.test.util.Utils;
  * @author zhanghanlin
  *
  */
-public class InsertionSort {
+public class InsertionSort<T extends Comparable<T>> {
 	
-	/**
-	 * 插入排序 - 升序
-	 * 伪代码
-	 * INSERTION-SORT(A)
-	 * 	for j = 2 to A.length
-	 * 		key = A[j]
-	 * 		//Insert A[j] into the sorted sequence A[1..j-1]
-	 * 		i = j - 1
-	 * 		while i > 0 and A[i] > key
-	 * 			A[i + 1] = A[i]
-	 * 			i = i - 1
-	 * 		A[i + 1] = key
-	 * @param src
-	 */
-	public static void insertionSort(Integer[] src) {
-		for (int i = 1; i < src.length; i++) {
-			if (src[i - 1] < src[i]) {
-				int temp = src[i];
-				int j = i;
-				while (j > 0 && src[j - 1] < temp) {
-					src[j] = src[j - 1];
-					j--;
-				}
-				src[j] = temp;
-			}
-		}
+	public InsertionSort() {
+		// TODO Auto-generated constructor stub
 	}
 	
 	/**
-	 * 插入排序 - 降序
+	 * 插入排序 - 算法导论代码
 	 * 伪代码
 	 * INSERTION-SORT(A)
 	 * 	for j = 2 to A.length
@@ -52,16 +28,43 @@ public class InsertionSort {
 	 * 		A[i + 1] = key
 	 * @param src
 	 */
-	public static void insertionSortAsc(Integer[] src) {
-		for (int i = 1; i < src.length; i++) {
-			if (src[i - 1] > src[i]) {
-				int temp = src[i];
+	public static <T extends Comparable<T>> T[] exampleSort(T[] t) {
+		for (int i = 1; i < t.length; i++) {
+			if (t[i - 1].compareTo(t[i]) < 0) {
+				T temp = t[i];
 				int j = i;
-				while (j > 0 && src[j - 1] > temp) {
-					src[j] = src[j - 1];
-					j--;
+				while (j > 0 && t[j - 1].compareTo(temp) < 0) {
+					t[j--] = t[j];
 				}
-				src[j] = temp;
+				t[j] = temp;
+			}
+		}
+		return t;
+	}
+	
+	/**
+	 * 优化排序
+	 * @param <T>
+	 * @param t
+	 * @return
+	 */
+	public static <T extends Comparable<T>> T[] sort(T[] t) {
+		int length = t.length;
+		for (int i = 0; i < length; i++) {
+			sort(t, i);
+		}
+		return t;
+	}
+	
+	private static <T extends Comparable<T>> void sort(T[] t, int i) {
+		for (int j = i; j > 0; j--) {
+			T element  = t[j];
+			T minElement = t[j - 1];
+			if (element.compareTo(minElement) < 0) {
+				t[j] = minElement;
+				t[j - 1] = element;
+			} else {
+				break;
 			}
 		}
 	}
@@ -69,9 +72,9 @@ public class InsertionSort {
 	public static void main(String[] args) {
 		Integer[] insertionSrc = Utils.random(10, 100, 10);
 		Utils.print(insertionSrc,"排序前");
-		insertionSort(insertionSrc);
-		Utils.print(insertionSrc,"排序后(降序)");
-		insertionSortAsc(insertionSrc);
-		Utils.print(insertionSrc,"排序后(升序)");
+		exampleSort(insertionSrc);
+		Utils.print(insertionSrc,"exampleSort倒序排序");
+		sort(insertionSrc);
+		Utils.print(insertionSrc,"sort正序排序");
 	}
 }
