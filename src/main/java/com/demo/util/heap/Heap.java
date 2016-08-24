8982969e0371a5b280d245d50a57b8e9908a503b
zhanghanlin <andyzhanghanlin@163.com>
2016-08-24 13:19:40 +0800

    堆

--- src/main/java/com/demo/util/heap/Heap.java
+++ src/main/java/com/demo/util/heap/Heap.java
@@ -36,11 +36,11 @@ public class Heap<T extends Comparable<T>> {
      * 堆排序
      * 伪代码
      * BUILDSORT(A)
-     * BUILD-MAX-HEAP(A)
-     * for i = A.length downto 2
-     * exchange A[1] with A[i]
-     * A.heap-size = A.heap-size - 1
-     * MAX-HEAPIFY(A, 1)
+     * --BUILD-MAX-HEAP(A)
+     * ----for i = A.length downto 2
+     * ------exchange A[1] with A[i]
+     * ------A.heap-size = A.heap-size - 1
+     * ------MAX-HEAPIFY(A, 1)
      *
      * @param type
      */
@@ -56,9 +56,9 @@ public class Heap<T extends Comparable<T>> {
      * 根据type得到最大/最小堆
      * 伪代码
      * BUILD-MAX-HEAP(A)
-     * A.heap-size = A.length
-     * for i = [A.length / 2] downto 1
-     * MAX-HEAPIFY(A, i)
+     * --A.heap-size = A.length
+     * --for i = [A.length / 2] downto 1
+     * ----MAX-HEAPIFY(A, i)
      *
      * @param type MAX,MIN
      */
@@ -73,16 +73,16 @@ public class Heap<T extends Comparable<T>> {
      * 最大/最小堆
      * 伪代码
      * MAX-HEAPIFY(A, i)
-     * l = LEFT(i)
-     * r = RIGHT(i)
-     * if l ≤ A.heap-size and A[l] > A[i]
-     * largest = l
-     * else largest = i
-     * if r ≤ A.heap-size and A[r] > A[i]
-     * largest = r
-     * if largest ≠ i
-     * exchange A[i] with A[largest]
-     * MAX-HEAPIFY(A, largest)
+     * --l = LEFT(i)
+     * --r = RIGHT(i)
+     * --if l ≤ A.heap-size and A[l] > A[i]
+     * ----largest = l
+     * --else largest = i
+     * --if r ≤ A.heap-size and A[r] > A[i]
+     * ----largest = r
+     * --if largest ≠ i
+     * ----exchange A[i] with A[largest]
+     * ----MAX-HEAPIFY(A, largest)
      *
      * @param i        下标
      * @param heapSize 堆长度
